8fa51fc61ee18a6de7df06ac0b51b4d4fb12c7da
nati-m-afw <it.natnael.mekonnen@gmail.com>
2022-03-05 20:55:57 +0300

    flutter: added starter files

---
+++ flutter-user-client/lib/presentation/core/app_widget.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/cupertino.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Hi");
+  }
+}
