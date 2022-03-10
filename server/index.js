1c64d455720b1ca38f81beebe6ecf3dd51f48ff7
jrermi <it.ermias.asmare@gmail.com>
2022-03-11 01:30:48 +0300

    add players route to index.js

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 // Import Libraries
 const express = require("express");
+require("dotenv").config();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
 // Development Supports
+const { printConsole } = require("./src/utils/development");
 
+const PORT = process.env.PORT || 3000;
 // Create express app
 const app = express();
 
@@ -13,6 +16,7 @@ const connectToDB = require("./src/config/db_config");
 connectToDB();
 
 // Import Routes
+const players = require("./src/routes/players.routes");
 
 // Import Middleware
 
@@ -22,6 +26,17 @@ app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors());
 
 // Add Routes to app
-
+app.use("/players", players);
 // Export app
-module.exports = app;
+// Run Node app
+app.listen(PORT, () =>
+  printConsole(
+    { data: `Server is live @${PORT}` },
+    { printLocation: "index.js:28" },
+    {
+      bgColor: "bgGreen",
+      textColor: "black",
+      underline: true,
+    }
+  )
+);
