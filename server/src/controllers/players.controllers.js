f60a2247396ab2c83b42a897faf55260801d5a64
jrermi <it.ermias.asmare@gmail.com>
2022-03-11 02:29:58 +0300

    fetching all players and one player by player id completed

--- server/src/controllers/players.controllers.js
+++ server/src/controllers/players.controllers.js
@@ -1,6 +1,6 @@
 //const asyncHandler = require("express-async-handler");
+const { collection } = require("../models/Players");
 const PlayerModel = require("../models/Players");
-
 
 async function addplayers (req,res){
     const { playerName, playerId, position, currentPrice} = req.body;
@@ -14,6 +14,44 @@ async function addplayers (req,res){
     res.send(`${playerName} is added to players.`);
 };
 
+
+// async function updateplayer (req,res){
+//     const { playerName, playerId, position, currentPrice} = req.body;
+//     const player = PlayerModel.find({playerId: req.params.playerId});
+//     const PID = req.params.playerId;
+//     if (!player) res.status(404).send('Player not found'); 
+   
+//         player.collection('players').update({_id: PID}, req.body)
+//            res.send(player)
+   
+   
+   
+    
+// };
+
+async function getplayer(req,res){
+    const player = await PlayerModel.findOne({ playerId: req.params.playerId});
+
+    res.send(player);
+
+};
+
+
+async function getplayers(req,res){
+const players = await PlayerModel.find();
+
+    res.send(players);
+
+};
+
+
+
+
+
 module.exports = {
     addplayers,
+    getplayer,
+    getplayers,
+    updateplayer
+
 }
