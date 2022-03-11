214c51c76a1f03d1edce48f708276bcacfc6cda4
jrermi <it.ermias.asmare@gmail.com>
2022-03-11 13:17:59 +0300

    configured route of team to index

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 // Import Libraries
 const express = require("express");
-const bodyParser = require("body-parser");
+require("dotenv").config();
 const cors = require("cors");
 
 // Development Supports
+const { printConsole } = require("./src/utils/development");
+
+// Import ENV Variables
+const PORT = process.env.PORT || 3000;
 
 // Create express app
 const app = express();
@@ -13,15 +17,27 @@ const connectToDB = require("./src/config/db_config");
 connectToDB();
 
 // Import Routes
+const teams = require("./src/routes/team.routes");
 
 // Import Middleware
 
 // Add Node Features
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors());
 
 // Add Routes to app
+app.use("/teams", teams);
 
-// Export app
-module.exports = app;
+// Run Node app
+app.listen(PORT, () =>
+  printConsole(
+    { data: `Server is live @${PORT}` },
+    { printLocation: "index.js:28" },
+    {
+      bgColor: "bgGreen",
+      textColor: "black",
+      underline: true,
+    }
+  )
+);
