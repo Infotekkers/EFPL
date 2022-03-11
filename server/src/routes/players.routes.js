9575442623e7261c9602f3886fc1afdb6342119b
jrermi <it.ermias.asmare@gmail.com>
2022-03-11 03:00:54 +0300

    added update route for playerupdate method

--- server/src/routes/players.routes.js
+++ server/src/routes/players.routes.js
@@ -6,5 +6,5 @@ const playersController = require("../controllers/players.controllers");
 router.post("/add", playersController.addplayers);
 router.get("/getplayers", playersController.getplayers);
 router.get("/getplayer/:playerId", playersController.getplayer);
-router.put("/updateplayer/:playerId", playersController.updateplayer);
+router.patch("/updateplayer/:playerId", playersController.updateplayer);
 module.exports = router;
