8ffc05a4533ee670f002b028beac5aeaf05a5e88
michael alemayehu <mikealexiv565@gmail.com>
2022-03-12 14:48:08 +0300

    stat model added
    -stat model
    -controller starter file
    -router starter file

---
+++ server/src/models/Stats2.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const saveSchema = mongoose.Schema({
+    playerId:{type:Number, required:true},
+    noOfSaves:{type:Number, required:true}
+})
+const fantasyScoresSchema = mongoose.Schema({
+    playerId:{type:Number, required:true},
+    playerScores:{type:Number, required:true}
+})
+const statSchema = mongoose.Schema({
+   fixtureId:{type:Number, required:true},
+   minutesPlayed :{type:[Number], required:true},
+   goalsScored :{type:[Number], required:true},
+   assists :{type:[Number], required:true},
+   yellows :{type:[Number], required:true},
+   reds:{type:[Number],required:true},
+   penaltiesMissed:{type:[Number], required:true},
+   saves:[saveSchema],
+   fantasyScores:[fantasyScoresSchema]
+   
+
+})
