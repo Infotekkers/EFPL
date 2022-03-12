e7a6d9ed3f6e25fae1099b681b43ee813fe2e3b8
michael alemayehu <mikealexiv565@gmail.com>
2022-03-12 17:12:08 +0300

    Updated stat schema

--- server/src/models/Stats2.js
+++ server/src/models/Stats2.js
@@ -1,24 +1,46 @@
 const mongoose = require('mongoose');
 
-// const saveSchema = mongoose.Schema({
-//     playerId:{type:Number, required:true},
-//     noOfSaves:{type:Number, required:true}
-// })
-// const fantasyScoresSchema = mongoose.Schema({
-//     playerId:{type:Number, required:true},
-//     playerScores:{type:Number, required:true}
-// })
+const minutesSchema = mongoose.Schema({
+    playerId:{type:Number, required:true},
+    noOfMinutes:{type:Number, required:true}
+})
+const goalsScoredSchema = mongoose.Schema({
+    playerId:{type:Number, required:true},
+    noOfGoals:{type:Number, required:true}
+})
+const assistsSchema = mongoose.Schema({
+   playerId:{type:Number, required:true},
+   noOfSaves:{type:Number, required:true}
+})
+const yellowsSchema = mongoose.Schema({
+   playerId:{type:Number, required:true},
+   noOfYellows:{type:Number, required:true}
+})
+const redsSchema = mongoose.Schema({
+   playerId:{type:Number, required:true},
+   noOfReds:{type:Number, required:true}
+})
+const penaltiesMissedSchema = mongoose.Schema({
+   playerId:{type:Number, required:true},
+   noOfPenMissed:{type:Number, required:true}
+})
+const savesSchema = mongoose.Schema({
+   playerId:{type:Number, required:true},
+   noOfSaves:{type:Number, required:true}
+})
+const fantasyScoresSchema = mongoose.Schema({
+   playerId:{type:Number, required:true},
+   playerScores:{type:Number, required:true}
+})
 const statSchema = mongoose.Schema({
    fixtureId:{type:Number, required:true},
-   minutesPlayed :{type:[Number], required:true},
-   goalsScored :{type:[Number], required:true},
-   assists :{type:[Number], required:true},
-   yellows :{type:[Number], required:true},
-   reds:{type:[Number],required:true},
-   penaltiesMissed:{type:[Number], required:true},
-   saves:{type:[Number], required:true},
-   fantasyScores:{type:[Number], required:true}
-   
-
+   minutesPlayed :[minutesSchema],
+   goalsScored :[goalsScoredSchema],
+   assists :[assistsSchema],
+   yellows :[yellowsSchema],
+   reds:[redsSchema],
+   penaltiesMissed:[penaltiesMissedSchema],
+   saves:[savesSchema],
+   fantasyScores:[fantasyScoresSchema]
 })
 module.exports = mongoose.model("stats", statSchema);
