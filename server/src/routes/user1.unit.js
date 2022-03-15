07832864f38d1a69d52172b54535336b4a0b1324
michael alemayehu <mikealexiv565@gmail.com>
2022-03-15 18:21:44 +0300

    fixed registration test

--- server/src/routes/user1.unit.js
+++ server/src/routes/user1.unit.js
@@ -10,27 +10,31 @@ const req = supertest(app);
 describe("Testing User ",()=>{
     test("POST /user1/register SUCCESS", async()=>{
         const reqBody = {
-            userName:"Ghost Boy",
-            password:"12345678@A1!",
-            email:"great@gmail.com",
-            teamName:"what",
-            country:"Eth"
-        };
+            
+                "userName":"wechdad",
+                "email":"fatadgaeonia@gmail.com",
+                "password":"12345679@gAl",
+                "teamName":"arrimish",
+                "country":"Ethiopia"
+            
+            };
         // clear db 
         await User.deleteMany();
 
-        reqBody.userId = String((await User.findOne()._id));
+        // reqBody.userId = String((await User.findOne()._id));
 
         // send request
         const res = await req.post("/user1/register").send(reqBody);
 
         // expect response
-        // expect(res.statusCode).toBe(201);
+        expect(res.statusCode).toBe(201);
         expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
-        // expect(res.reqBody).toMatchObject({message:"successfully registered"});
-
-        test("Close DB", async()=>{
-            mongoose.connection.close();
-        })
+        
+    
+        
     });
+    // test("POST")
+    test("Close DB", async()=>{
+        mongoose.connection.close();
+    })
 })
