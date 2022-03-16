b3465f31d6864a6206ff0aae6a86c3a6224f1929
jrermi <it.ermias.asmare@gmail.com>
2022-03-16 12:32:43 +0300

    update score and add score functions added

--- server/src/controllers/players.controllers.js
+++ server/src/controllers/players.controllers.js
@@ -1,21 +1,45 @@
-//const asyncHandler = require("express-async-handler");
+const asyncHandler = require("express-async-handler");
 const { collection } = require("../models/Players");
 const PlayerModel = require("../models/Players");
 
-async function addplayers (req,res){
-    const { playerName, playerId, position, currentPrice} = req.body;
- 
+const addplayers = asyncHandler(async (req,res) =>{
+    const { playerName, playerId, eplTeamId,position,currentPrice,score,availablity,history} = req.body;
     await new PlayerModel ({
         playerName,
-        playerId,
+        playerId, //range increment 
         position,
         currentPrice,
+        score,
+        history,
+        availablity,
+        eplTeamId,
     }).save();
     res.send(`${playerName} is added to players.`);
-};
+});
 
+const addScore = asyncHandler(async(req,res) => {
+    const { score } = req.body;
+    const player= await PlayerModel
+    .updateMany({playerId:req.params.playerId},{
+        $push: {
+            score: score
+        }
+    })
+    res.send("new score added")
+})
 
-async function updateplayer (req,res){
+const updateScore = asyncHandler(async(req,res) => {
+    const { score } = req.body;
+    const player = await PlayerModel
+    .updateOne({playerId:req.params.playerId},{
+        $set: {
+           score:score
+        }
+    })
+    res.send("updated")
+})
+
+const updateplayer = asyncHandler(async(req,res) =>{
     const { playerName, playerId, position, currentPrice} = req.body;
     const player = await PlayerModel
     .updateOne({ playerId: req.params.playerId}, {
@@ -24,38 +48,38 @@ async function updateplayer (req,res){
             position: position,
             playerId: playerId,
             currentPrice:currentPrice,
+        
         }
     });
-    res.send(player);
-   
-    
-};
-
-async function getplayer(req,res){
-    
-    const players = await PlayerModel.find( {playerId: req.params.playerId});
-
-    res.send(players);
-
-   
-};
+    res.send(player); 
+});
 
 
-async function getplayers(req,res){
+const getplayer = asyncHandler(async(req,res) =>{
+    const players = await PlayerModel.find({playerId: req.params.playerId});
+    res.send(players); 
+});
+
+
+const getplayers = asyncHandler (async(req,res) =>{
 const players = await PlayerModel.find();
-
     res.send(players);
+});
 
-};
-
-
-
+const deleteplayer = asyncHandler(async(req,res)=>{
+    const player = await PlayerModel.deleteOne({playerId: req.params.playerId});
+    res.send(`player ${player.playerName} is removed`);
+  
+  
+});
 
 
 module.exports = {
     addplayers,
     getplayer,
     getplayers,
-    updateplayer
-
+    updateplayer,
+    deleteplayer,
+    updateScore,
+    addScore
 }
