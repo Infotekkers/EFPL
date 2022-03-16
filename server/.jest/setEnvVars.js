6d48487fc9ec95edac3a051780bc13e21f5830bf
BarokDG <it.barok.dagim@gmail.com>
2022-03-16 20:42:53 +0300

    Fix environment variable

--- server/.jest/setEnvVars.js
+++ server/.jest/setEnvVars.js
@@ -2,4 +2,4 @@ process.env.EXPRESS_ENV = "development";
 process.env.PORT = "5000";
 process.env.JWT_SECRET = "dsajdklsadjlsajkldsajdklas";
 process.env.MONGO_REMOTE_URL = "";
-process.env.MONGO_LOCAL_URL = "mongodb://localhost:27017/EFPL";
+process.env.MONGO_LOCAL_URL = "mongodb://localhost:27017";
