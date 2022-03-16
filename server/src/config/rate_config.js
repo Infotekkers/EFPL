714d18450694525c446407e2b8a4461cbad7619a
BarokDG <it.barok.dagim@gmail.com>
2022-03-16 21:13:19 +0300

    Fix merge conflicts

--- server/src/config/rate_config.js
+++ server/src/config/rate_config.js
@@ -6,7 +6,7 @@ const allowlist = ["::1"];
 
 const rateLimiterConfig = rateLimit({
   windowMs: 60 * 1000, // 15 minutes
-  max: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  max: 1000, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   message: "Maximum Request Limit Rate Reached.",
