4b8f944ffb30b47ecb115e99d03edd29a40d7ae2
jrermi <it.ermias.asmare@gmail.com>
2022-03-16 12:32:06 +0300

    all schema of players added

--- server/src/models/players.js
+++ server/src/models/players.js
@@ -5,57 +5,54 @@ const availabilitySchema = mongoose.Schema({
   injuryMessage: { type: String, default: "" },
 });
 
-// const scoreSchema = mongoose.Schema({
-//   gameweekId: Number,
-//   price: Number,
-//   fantasyScore: Number,
-//   minutesPlayed: Number,
-//   goal: Number,
-//   assists: Number,
-//   cleanSheet: Number,
-//   yellows: Number,
-//   reds: Number,
-//   penalitiesMissed: Number,
-//   penalitiesSaved: Number,
-//   saves: Number,
-//   ownGoal: Number,
-//   transfersIn: Number,
-//   transfersOut: Number,
-//   form: Number,
-// });
+const scoreSchema = mongoose.Schema({
+  gameweekId: Number,
+  price: Number,
+  fantasyScore: Number,
+  minutesPlayed: Number,
+  goal: Number,
+  assists: Number,
+  cleanSheet: Number,
+  yellows: Number,
+  reds: Number,
+  penalitiesMissed: Number,
+  penalitiesSaved: Number,
+  saves: Number,
+  ownGoal: Number,
+  transfersIn: Number,
+  transfersOut: Number,
+  form: Number,
+});
 
-// const historySchema = mongoose.Schema({
-//   startingPrice: Number,
-//   endingPrice: Number,
-//   totalFanstasyScore: Number,
-//   totalMinutesScore: Number,
-//   totalGoals: Number,
-//   totalAssists: Number,
-//   totalCleanSheets: Number,
-//   totalYellows: Number,
-//   totalReds: Number,
-//   totalPenalitiesMissed: Number,
-//   totalPenalitiesSaved: Number,
-//   totalSaves: Number,
-//   totalOwnGoal: Number,
-//   totalTransfersIn: Number,
-//   totalTransfersOut: Number,
-//   totalForm: Number,
-// });
+const historySchema = mongoose.Schema({
+  startingPrice: Number,
+  endingPrice: Number,
+  totalFanstasyScore: Number,
+  totalMinutesScore: Number,
+  totalGoals: Number,
+  totalAssists: Number,
+  totalCleanSheets: Number,
+  totalYellows: Number,
+  totalReds: Number,
+  totalPenalitiesMissed: Number,
+  totalPenalitiesSaved: Number,
+  totalSaves: Number,
+  totalOwnGoal: Number,
+  totalTransfersIn: Number,
+  totalTransfersOut: Number,
+  totalForm: Number,
+});
 
 const playerSchema = mongoose.Schema({
   playername: String,
-  //eplTeamId: String,
+  eplTeamId: String,
   currentPrice: Number,
   position: String,
   playerId: Number,
 
- // availability: availabilitySchema,
-  //score: [scoreSchema],
-  //history: {
-  //  type: Map,
-    //of: historySchema,
-  //},
+ availability: {type: [availabilitySchema]},
+  score: {type: [scoreSchema]},
+  history: {type: [historySchema]}
 });
 
 module.exports = mongoose.model("players", playerSchema);
