7f889ecbc88aba22f036f377ac0ac4f67d131dc2
jrermi <it.ermias.asmare@gmail.com>
2022-03-17 21:44:25 +0300

    added delete route to team.controller.js

--- server/src/routes/team.routes.js
+++ server/src/routes/team.routes.js
@@ -7,6 +7,6 @@ router.post("/add", teamsController.addteam);
 router.get("/getteams", teamsController.getteams);
 router.get("/getteam/:teamId", teamsController.getteam);
 router.patch("/updateteam/:teamId", teamsController.updateteam);
-//router.delete("/deleteteam/:teamId", teamsController.deleteteam);
+router.delete("/deleteteam/:teamId", teamsController.deleteteam);
 
 module.exports = router;
