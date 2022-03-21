0be906593a078a9b301269779eb5e3ef2edfdccf
nati-m-afw <it.natnael.mekonnen@gmail.com>
2022-03-21 17:25:06 +0300

    lint fix

--- server/src/controllers/players.controllers.js
+++ server/src/controllers/players.controllers.js
@@ -1,108 +1,125 @@
 const asyncHandler = require("express-async-handler");
-const { collection } = require("../models/Players");
 const PlayerModel = require("../models/Players");
 
-const addplayers = asyncHandler(async (req,res) =>{
-    const { playerName, eplTeamId,position,currentPrice,score,availability,history} = req.body;
-        await new PlayerModel ({
-            playerName,
-            position,
-            currentPrice,
-            score,
-            history,
-            availability,
-            eplTeamId,
-        }).save();
-        res.status(201).send(`${playerName} added successfully`);
+const addplayers = asyncHandler(async (req, res) => {
+  const {
+    playerName,
+    eplTeamId,
+    position,
+    currentPrice,
+    score,
+    availability,
+    history,
+  } = req.body;
+  await new PlayerModel({
+    playerName,
+    position,
+    currentPrice,
+    score,
+    history,
+    availability,
+    eplTeamId,
+  }).save();
+  res.status(201).send(`${playerName} added successfully`);
 });
-//Questions eplTeamId -  what is that
-const updateplayer = asyncHandler(async(req,res) =>{
-    const { playerName,history,eplTeamId,availablity, playerId, position, currentPrice} = req.body; 
+// Questions eplTeamId -  what is that
+const updateplayer = asyncHandler(async (req, res) => {
+  const {
+    playerName,
+    history,
+    eplTeamId,
+    availablity,
+    playerId,
+    position,
+    currentPrice,
+  } = req.body;
 
-    
-    // const verifyPlayer =  await PlayerModel.findOne({playerId:req.params.playerId})
-    // if(verifyPlayer){
-        const player = await PlayerModel
-        .updateOne({ playerId: req.body.playerId}, {
-            $set: {
-                playerName: playerName,
-                position: position,
-                playerId: playerId,
-                currentPrice:currentPrice,
-                history:history,
-                eplTeamId:eplTeamId,
-                availablity:availablity
-            }
-        });
-       
-        res.status(201).send(`Info updated successful`);
-    // } else{
-    //     return res.status(404).send(`player doesn exist`);
-    // }
-    
-    
-   
-}); 
+  // const verifyPlayer =  await PlayerModel.findOne({playerId:req.params.playerId})
+  // if(verifyPlayer){
+  await PlayerModel.updateOne(
+    { playerId: req.body.playerId },
+    {
+      $set: {
+        playerName: playerName,
+        position: position,
+        playerId: playerId,
+        currentPrice: currentPrice,
+        history: history,
+        eplTeamId: eplTeamId,
+        availablity: availablity,
+      },
+    }
+  );
 
-
-const updateScore = asyncHandler(async(req,res) => {
-    const { score } = req.body;
-    const verifyPlayer =  await PlayerModel.findOne({playerId:req.params.playerId})
-    console.log(verifyPlayer);
-    // if(verifyPlayer){
-    const scorearray = verifyPlayer.score;
-    const Gameweek = scorearray.filter(s=>s.gameweekId == req.params.gameweekId);
-    const index = scorearray.indexOf(Gameweek[0]);
-    scorearray[index] = score;
-    verifyPlayer.score == scorearray;
-    console.log(verifyPlayer);
-    await verifyPlayer.save();
-        res.status(201).send(`Score for Gameweek update successful`);
-    //  }else{
-    //     res.status(404).send(`player ${verifyPlayer.playerName} doesn exist`);
-    // } 
+  res.status(201).send(`Info updated successful`);
+  // } else{
+  //     return res.status(404).send(`player doesn exist`);
+  // }
 });
 
-
-const addScore = asyncHandler(async(req,res) => {
-    const { score } = req.body; 
-    const verifyPlayer =  await PlayerModel.findOne({playerId:req.params.playerId})
-    // if(verifyPlayer){
-        const player= await PlayerModel
-        .updateMany({playerId:req.params.playerId},{
-            $push: {
-                score: score
-            }
-        })
-        res.status(201).send(`Score added successfully`);
-    // }else{
-    //     res.status(404).send(`player ${verifyPlayer.playerName} doesn exist`);
-    // }
+const updateScore = asyncHandler(async (req, res) => {
+  const { score } = req.body;
+  const verifyPlayer = await PlayerModel.findOne({
+    playerId: req.params.playerId,
+  });
+  console.log(verifyPlayer);
+  // if(verifyPlayer){
+  const scorearray = verifyPlayer.score;
+  const Gameweek = scorearray.filter(
+    (s) => s.gameweekId === req.params.gameweekId
+  );
+  const index = scorearray.indexOf(Gameweek[0]);
+  scorearray[index] = score;
+  verifyPlayer.score = scorearray;
+  console.log(verifyPlayer);
+  await verifyPlayer.save();
+  res.status(201).send(`Score for Gameweek update successful`);
+  //  }else{
+  //     res.status(404).send(`player ${verifyPlayer.playerName} doesn exist`);
+  // }
 });
 
-
-const getplayer = asyncHandler(async(req,res) =>{
-    const players = await PlayerModel.find({playerId: req.params.playerId});
-    res.send(players); 
+const addScore = asyncHandler(async (req, res) => {
+  const { score } = req.body;
+  //   const verifyPlayer = await PlayerModel.findOne({
+  //     playerId: req.params.playerId,
+  //   });
+  // if(verifyPlayer){
+  await PlayerModel.updateMany(
+    { playerId: req.params.playerId },
+    {
+      $push: {
+        score: score,
+      },
+    }
+  );
+  res.status(201).send(`Score added successfully`);
+  // }else{
+  //     res.status(404).send(`player ${verifyPlayer.playerName} doesn exist`);
+  // }
 });
 
-
-const getplayers = asyncHandler (async(req,res) =>{
-const players = await PlayerModel.find();
-    res.send(players);
+const getplayer = asyncHandler(async (req, res) => {
+  const players = await PlayerModel.find({ playerId: req.params.playerId });
+  res.send(players);
 });
 
-const deleteplayer = asyncHandler(async(req,res)=>{
-    const player = await PlayerModel.deleteOne({playerId: req.params.playerId});
-    res.send(`player ${player.playerName} is removed`);
+const getplayers = asyncHandler(async (req, res) => {
+  const players = await PlayerModel.find();
+  res.send(players);
+});
+
+const deleteplayer = asyncHandler(async (req, res) => {
+  const player = await PlayerModel.deleteOne({ playerId: req.params.playerId });
+  res.send(`player ${player.playerName} is removed`);
 });
 
 module.exports = {
-    addplayers,
-    getplayer,
-    getplayers,
-    updateplayer,
-    deleteplayer,
-    updateScore,
-    addScore
-}
+  addplayers,
+  getplayer,
+  getplayers,
+  updateplayer,
+  deleteplayer,
+  updateScore,
+  addScore,
+};
--- server/src/models/players.js
+++ server/src/models/players.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
-const autoIncrement = require('mongoose-auto-increment');
+const autoIncrement = require("mongoose-auto-increment");
 
-var connection = mongoose.createConnection("mongodb://localhost:27017/FPLDB");
+const connection = mongoose.createConnection("mongodb://localhost:27017/FPLDB");
 autoIncrement.initialize(connection);
 
 const availabilitySchema = mongoose.Schema({
@@ -10,60 +10,60 @@ const availabilitySchema = mongoose.Schema({
 });
 
 const scoreSchema = mongoose.Schema({
-  gameweekId:{type:Number, },
-  price: {type:Number, },
-  fantasyScore: {type:Number, },
-  minutesPlayed:{type:Number, },
-  goal:{type:Number, },
-  assists:{type:Number, },
-  cleanSheet: {type:Number, },
-  yellows: {type:Number, },
-  reds: {type:Number, },
-  penalitiesMissed:{type:Number, },
-  penalitiesSaved: {type:Number, },
-  saves: {type:Number, },
-  ownGoal: {type:Number, },
-  transfersIn: {type:Number, },
-  transfersOut:{type:Number, },
-  form: {type:Number, },
+  gameweekId: { type: Number },
+  price: { type: Number },
+  fantasyScore: { type: Number },
+  minutesPlayed: { type: Number },
+  goal: { type: Number },
+  assists: { type: Number },
+  cleanSheet: { type: Number },
+  yellows: { type: Number },
+  reds: { type: Number },
+  penalitiesMissed: { type: Number },
+  penalitiesSaved: { type: Number },
+  saves: { type: Number },
+  ownGoal: { type: Number },
+  transfersIn: { type: Number },
+  transfersOut: { type: Number },
+  form: { type: Number },
 });
 
 const historySchema = mongoose.Schema({
-  startingPrice: {type:Number, },
-  endingPrice: {type:Number, },
-  totalFanstasyScore:{type:Number, },
-  totalMinutesScore: {type:Number, },
-  totalGoals: {type:Number, },
-  totalAssists: {type:Number, },
-  totalCleanSheets: {type:Number, },
-  totalYellows:{type:Number, },
-  totalReds: {type:Number, },
-  totalPenalitiesMissed:{type:Number, },
-  totalPenalitiesSaved:{type:Number, },
-  totalSaves:{type:Number, },
-  totalOwnGoal: {type:Number, },
-  totalTransfersIn:{type:Number, },
-  totalTransfersOut: {type:Number, },
-  totalForm: {type:Number, },
+  startingPrice: { type: Number },
+  endingPrice: { type: Number },
+  totalFanstasyScore: { type: Number },
+  totalMinutesScore: { type: Number },
+  totalGoals: { type: Number },
+  totalAssists: { type: Number },
+  totalCleanSheets: { type: Number },
+  totalYellows: { type: Number },
+  totalReds: { type: Number },
+  totalPenalitiesMissed: { type: Number },
+  totalPenalitiesSaved: { type: Number },
+  totalSaves: { type: Number },
+  totalOwnGoal: { type: Number },
+  totalTransfersIn: { type: Number },
+  totalTransfersOut: { type: Number },
+  totalForm: { type: Number },
 });
 
 const playerSchema = mongoose.Schema({
-  playerName: {type:String, },
-  eplTeamId: {type:String, },
-  currentPrice:{type:Number, },
-  position: {type:String, },
- availability: {type: [availabilitySchema]},
-  score: {type: [scoreSchema]},
-  history: {type: [historySchema]}
-}); 
+  playerName: { type: String },
+  eplTeamId: { type: String },
+  currentPrice: { type: Number },
+  position: { type: String },
+  availability: { type: [availabilitySchema] },
+  score: { type: [scoreSchema] },
+  history: { type: [historySchema] },
+});
 
-var player = connection.model('player', playerSchema);
+const player = connection.model("player", playerSchema);
 
-playerSchema.plugin(autoIncrement.plugin, 
-        { model: player, 
-          field:'playerId', 
-          startAt:100000, 
-          incrmentBy: 1
-        });
+playerSchema.plugin(autoIncrement.plugin, {
+  model: player,
+  field: "playerId",
+  startAt: 100000,
+  incrmentBy: 1,
+});
 
 module.exports = mongoose.model("players", playerSchema);
