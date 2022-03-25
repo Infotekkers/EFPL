70b4c88c638585e0850183e55218d81823349c84
thomasalemayehu <61502533+thomasalemayehu@users.noreply.github.com>
2022-03-25 09:19:33 +0300

    Added Notification Component

--- vue-admin-client/src/views/HomeView.vue
+++ vue-admin-client/src/views/HomeView.vue
@@ -2,17 +2,24 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <NotificationComponent
+      message="A global Notification System Message here."
+      type="success"
+      :showNotification="true"
+    />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import NotificationComponent from "@/components/NotificationComponent";
 
 export default {
   name: "HomeView",
   components: {
     HelloWorld,
+    NotificationComponent,
   },
 };
 </script>
