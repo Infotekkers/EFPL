7108d139dd93e25f3aff3b2593cf8eff1a1e483c
jrermi <it.ermias.asmare@gmail.com>
2022-04-04 11:26:33 +0300

    created crud logics on vuex store for player

--- vue-admin-client/src/store/player.store.js
+++ vue-admin-client/src/store/player.store.js
@@ -6,25 +6,40 @@ const baseURL = process.env.VUE_APP_API_BASE_URL;
 export default {
   namespaced: true,
   state: {
-    players: [],
-    player: "",
-  },
+    allPlayers: [],
+    allPlayersUnfiltered: [],
 
+    eplTeamId: "",
+    imageChanged: false,
+  },
   mutations: {
-    SAVE_GRADES(state, payload) {
-      state.players = payload;
+    SET_ALL_PLAYERS(state, payload) {
+      state.allPlayers = payload;
+      state.allPlayersUnfiltered = payload;
     },
-    SAVE_GRADE(state, payload) {
-      state.player = payload;
+    SET_FILTERED_PLAYERS(state, payload) {
+      state.allPlayers = payload;
+    },
+    SET_EDIT_PLAYER_ID(state, payload) {
+      state.editPlayerId = payload;
+    },
+    SET_IMAGE_CHANGED_STATUS(state, payload) {
+      state.imageChanged = payload;
     },
   },
   actions: {
-    async getallplayers({ commit }) {
+    setEditPlayerId(context, playerId) {
+      context.commit("SET_EDIT_PLAYER_ID", playerId);
+    },
+    setImageChanged(context, status) {
+      context.commit("SET_IMAGE_CHANGED_STATUS", status);
+    },
+    async setAllPlayers(context) {
       await axios
         .get(`${baseURL}/players/getplayers`)
         .then((res) => {
           if (res.status === 200) {
-            commit("SAVE_GRADES", res.data);
+            context.commit("SET_ALL_PLAYERS", res.data);
           }
         })
         .catch((err) => {
@@ -35,16 +50,18 @@ export default {
           });
         });
     },
-    async addPlayer(context, player) {
+    async savePlayer(context, playerData) {
       await axios
-        .post(`${baseURL}/players/addplayer`, player)
+        .post(`${baseURL}/players/addplayer`, playerData)
         .then(async (res) => {
           if (res.status === 200) {
             this.$store.dispatch("Global/setNotificationInfo", {
               showNotification: true,
               notificationType: "success",
-              notificationMessage: `Player ${player.playerName} added succesfully`,
+              notificationMessage: `Player ${playerData.playerName} added succesfully`,
             });
+
+            store.dispatch("player/setAllPlayers");
           }
         })
         .catch((err) => {
@@ -52,18 +69,28 @@ export default {
             showNotification: true,
             notificationType: "error",
             notificationMessage: `${err.response.data}`,
-            notificationDuration: 8000,
+          });
+        });
+    },
+    async deletePlayer(context, playerId) {
+      axios
+        .delete(`${baseURL}/players/deleteplayer/${playerId}`)
+        .then((response) => {
+          store.dispatch("Global/setNotificationInfo", {
+            showNotification: true,
+            notificationType: "success",
+            notificationMessage: response.data,
+          });
+          store.dispatch("Player/setAllPlayers");
+        })
+        .catch((err) => {
+          store.dispatch("Global/setNotificationInfo", {
+            showNotification: true,
+            notificationType: "error",
+            notificationMessage: err.response.data,
           });
         });
     },
   },
-  getters: {
-    getAllPlayers: function (state) {
-      return state.players;
-    },
-
-    getPlayer: function (state) {
-      return state.player;
-    },
-  },
+  getters: {},
 };
