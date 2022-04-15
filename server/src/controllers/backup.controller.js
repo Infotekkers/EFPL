5c45119aed696373a5cb858d44a3603ddb6e3b16
thomasalemayehu <61502533+thomasalemayehu@users.noreply.github.com>
2022-04-15 21:47:41 +0300

    season end added

--- server/src/controllers/backup.controller.js
+++ server/src/controllers/backup.controller.js
@@ -1,55 +1,149 @@
 const asyncHandler = require("express-async-handler");
 const fs = require("fs");
 
-// const mongoose = require("mongoose");
 const Teams = require("../models/Teams");
 const Players = require("../models/Player");
 const Fixture = require("../models/Fixtures");
+
+const Backup = require("../models/Backup");
+const { moveFile } = require("../utils/helpers");
 //   );
 const backup = asyncHandler(async (req, res) => {
-  // mongoose.connection.close();
+  const seasonId = "21/22";
+  const currentSeasonTeams = await Teams.find();
+  const currentSeasonPlayers = await Players.find();
+  const currentSeasonFixtures = await Fixture.find();
 
-  const team = await Teams.find();
-  const player = await Players.find();
-  const fixture = await Fixture.find();
-  const season = req.params.id;
+  currentSeasonTeams.forEach(async (team) => {
+    const verifyExistence = await Backup.teamBkModel.find({
+      teamName: team.teamName,
+    });
+    if (verifyExistence.length > 0) {
+      await Backup.teamBkModel.deleteOne({ teamName: team.teamName });
+    }
 
-  try {
-    fs.writeFileSync(
-      `src/controllers/f/${season}-player.json`,
-      JSON.stringify(player)
-    );
-    fs.writeFileSync(
-      `src/controllers/f/${season}-team.json`,
-      JSON.stringify(team)
-    );
-    fs.writeFileSync(
-      `src/controllers/f/${season}-fixture.json`,
-      JSON.stringify(fixture)
+    const teamName = team.teamName;
+    const teamCity = team.teamCity;
+    const teamStadium = team.teamStadium;
+    const teamLogo = team.teamLogo;
+    const stadiumCapacity = team.stadiumCapacity;
+    const foundedIn = team.foundedIn;
+    const teamCoach = team.teamCoach;
+    console.log(teamLogo);
+
+    const fileName = teamLogo.split("/");
+
+    // Move logo here
+    await moveFile(
+      "." + teamLogo,
+      `./uploads/legacy/teams/${fileName[fileName.length - 1]}`
     );
 
-    res.json("success");
-  } catch (err) {
-    res.send(err);
-    console.log(err);
-  }
+    await Backup.teamBkModel.create({
+      teamName,
+      teamCity,
+      teamStadium,
+      teamLogo,
+      stadiumCapacity,
+      foundedIn,
+      teamCoach,
+    });
+  });
+
+  currentSeasonPlayers.forEach(async (player) => {
+    const verifyExistence = await Backup.playersBkModel.find({
+      playerName: player.playerName,
+      eplTeamId: player.eplTeamId,
+      position: player.position,
+    });
+
+    // TODO:Change Model
+    const playerName = player.playerName;
+    const eplTeamId = player.eplTeamId;
+    const currentPrice = player.currentPrice;
+    const position = player.position;
+    const availability = player.availability;
+    const score = player.score;
+    const history = player.history;
+
+    // Move logo here
+    // const logoMove = await moveFile(
+    //   "." + teamLogo,
+    //   `./uploads/legacy/teams/${fileName[fileName.length - 1]}`
+    // );
+
+    if (verifyExistence.length > 0) {
+      await Backup.playersBkModel.delete({
+        playerName: player.playerName,
+        eplTeamId: player.eplTeamId,
+        position: player.position,
+      });
+    }
+
+    await Backup.playersBkModel.create({
+      playerName,
+      eplTeamId,
+      currentPrice,
+      position,
+      availability,
+      score,
+      history,
+    });
+  });
+
+  currentSeasonFixtures.forEach(async (fixture) => {
+    const verifyExistence = await Backup.playersBkModel.find({
+      gameweekId: fixture.gameweekId,
+      matchId: fixture.matchId,
+    });
+
+    if (verifyExistence > 0) {
+      await Backup.playersBkModel.delete({
+        gameweekId: fixture.gameweekId,
+        matchId: fixture.matchId,
+      });
+    }
+
+    const gameweekId = fixture.gameweekId;
+    const matchId = fixture.matchId;
+    const schedule = fixture.schedule;
+    const status = fixture.status;
+
+    const homeTeam = fixture.homeTeam;
+    const awayTeam = fixture.awayTeam;
+    const homeTeamLineUp = fixture.homeTeamLineUp;
+    const awayTeamLineUp = fixture.awayTeamLineUp;
+    const matchStat = fixture.matchStatSchema;
+
+    await Backup.fixtureBkModel.create({
+      seasonId,
+      gameweekId,
+      matchId,
+      schedule,
+      status,
+      homeTeam,
+      awayTeam,
+      homeTeamLineUp,
+      awayTeamLineUp,
+      matchStat,
+    });
+  });
+
+  res.send(currentSeasonTeams);
 });
 const restore = asyncHandler(async (req, res) => {
   const season = req.params.id;
- try{
+  try {
     const data = fs.readFileSync(`src/controllers/f/${season}-team.json`);
     const docs = JSON.parse(data.toString());
     res.send(docs);
- }catch(err){
-    
-     if(err.errno===-4058){
-         
-         res.status(422).json({message:`No data for season ${season}`})
-     }
-    else{
-        res.send(err)
+  } catch (err) {
+    if (err.errno === -4058) {
+      res.status(422).json({ message: `No data for season ${season}` });
+    } else {
+      res.send(err);
     }
- }
+  }
 });
 
 module.exports = { backup, restore };
---
+++ server/src/models/Backup.js
@@ -0,0 +1,335 @@
+const mongoose = require("mongoose");
+
+const MONGO_LEGACY_DB_URI = process.env.MONGO_LOCAL_LEGACY_URL;
+
+// Create Connection
+const bkConnection = mongoose.createConnection(MONGO_LEGACY_DB_URI);
+
+const autoIncrement = require("mongoose-auto-increment");
+
+autoIncrement.initialize(mongoose.connection);
+
+/*
+=======================================
+Teams
+=======================================
+*/
+const teamSchema = mongoose.Schema({
+  teamName: {
+    type: String,
+    required: [true, "Custom Error - Required Value *:Team name is required."],
+  },
+  teamCity: {
+    type: String,
+    required: [true, "Custom Error - Required Value *:Team city is required."],
+  },
+  teamStadium: {
+    type: String,
+    required: [
+      true,
+      "Custom Error - Required Value *:Team stadium is required.",
+    ],
+  },
+  teamLogo: {
+    type: String,
+    required: [true, "Custom Error - Required Value *:Team logo is required."],
+  },
+  stadiumCapacity: {
+    type: Number,
+  },
+  foundedIn: {
+    type: Number,
+  },
+  teamCoach: {
+    type: String,
+  },
+});
+
+teamSchema.plugin(autoIncrement.plugin, {
+  model: "teams",
+  field: "teamId",
+  startAt: 1,
+  incrementBy: 1,
+});
+
+/*
+=======================================
+Players
+=======================================
+*/
+const availabilitySchema = mongoose.Schema({
+  injuryStatus: String,
+  injuryMessage: { type: String, default: "" },
+});
+
+const scoreSchema = mongoose.Schema({
+  gameweekId: { type: Number },
+  price: { type: Number },
+  fantasyScore: { type: Number },
+  minutesPlayed: { type: Number },
+  goals: { type: Number },
+  assists: { type: Number },
+  cleanSheet: { type: Number },
+  yellows: { type: Number },
+  reds: { type: Number },
+  penalitiesMissed: { type: Number },
+  penalitiesSaved: { type: Number },
+  saves: { type: Number },
+  ownGoal: { type: Number },
+  transfersIn: { type: Number },
+  transfersOut: { type: Number },
+  form: { type: Number },
+});
+
+const historySchema = mongoose.Schema({
+  startingPrice: { type: Number },
+  endingPrice: { type: Number },
+  totalFanstasyScore: { type: Number },
+  totalMinutesScore: { type: Number },
+  totalGoals: { type: Number },
+  totalAssists: { type: Number },
+  totalCleanSheets: { type: Number },
+  totalYellows: { type: Number },
+  totalReds: { type: Number },
+  totalPenalitiesMissed: { type: Number },
+  totalPenalitiesSaved: { type: Number },
+  totalSaves: { type: Number },
+  totalOwnGoal: { type: Number },
+  totalTransfersIn: { type: Number },
+  totalTransfersOut: { type: Number },
+  totalForm: { type: Number },
+});
+
+const playerSchema = mongoose.Schema({
+  playerName: { type: String },
+  eplTeamId: { type: String },
+  currentPrice: { type: Number },
+  position: { type: String },
+  availability: { type: [availabilitySchema] },
+  score: { type: [scoreSchema] },
+  history: { type: [historySchema] },
+});
+
+playerSchema.plugin(autoIncrement.plugin, {
+  model: "players",
+  field: "playerId",
+  startAt: 100000,
+  incrementBy: 1,
+});
+
+/*
+=======================================
+Fixtures
+=======================================
+*/
+
+const minutesSchema = mongoose.Schema({
+  playerId: {
+    type: Number,
+  },
+  noOfMinutes: {
+    type: Number,
+    default: 0,
+  },
+});
+const goalsScoredSchema = mongoose.Schema({
+  playerId: {
+    type: Number,
+  },
+  noOfGoals: {
+    type: Number,
+    default: 0,
+  },
+});
+const assistsSchema = mongoose.Schema({
+  playerId: {
+    type: Number,
+  },
+  noOfAssists: {
+    type: Number,
+    default: 0,
+  },
+});
+const cleanSheetSchema = mongoose.Schema({
+  playerId: {
+    type: Number,
+  },
+  noOfCleanSheets: {
+    type: Number,
+    default: 0,
+  },
+});
+const yellowsSchema = mongoose.Schema({
+  playerId: {
+    type: Number,
+  },
+  noOfYellows: {
+    type: Number,
+    default: 0,
+  },
+});
+const redsSchema = mongoose.Schema({
+  playerId: {
+    type: Number,
+  },
+  noOfReds: {
+    type: Number,
+    default: 0,
+  },
+});
+const penaltiesMissedSchema = mongoose.Schema({
+  playerId: {
+    type: Number,
+  },
+  noOfPenMissed: {
+    type: Number,
+    default: 0,
+  },
+});
+const penaltiesSavedSchema = mongoose.Schema({
+  playerId: {
+    type: Number,
+  },
+  noOfPenSaved: {
+    type: Number,
+    default: 0,
+  },
+});
+const savesSchema = mongoose.Schema({
+  playerId: {
+    type: Number,
+  },
+  noOfSaves: {
+    type: Number,
+    default: 0,
+  },
+});
+const ownGoalSchema = mongoose.Schema({
+  playerId: {
+    type: Number,
+  },
+  noOfOwnGoals: {
+    type: Number,
+    default: 0,
+  },
+});
+const fantasyScoresSchema = mongoose.Schema({
+  playerId: {
+    type: Number,
+  },
+  playerScores: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const matchStatSchema = mongoose.Schema({
+  minutesPlayed: { type: Map, of: minutesSchema },
+  goalsScored: { type: Map, of: goalsScoredSchema },
+  assists: { type: Map, of: assistsSchema },
+  cleanSheet: { type: Map, of: cleanSheetSchema },
+  yellows: { type: Map, of: yellowsSchema },
+  reds: { type: Map, of: redsSchema },
+  penaltiesMissed: { type: Map, of: penaltiesMissedSchema },
+  penaltiesSaved: { type: Map, of: penaltiesSavedSchema },
+  saves: { type: Map, of: savesSchema },
+  ownGoal: { type: Map, of: ownGoalSchema },
+  fantasyScores: { type: Map, of: fantasyScoresSchema },
+});
+
+// used within fixtureSchema below
+const lineupSchema = mongoose.Schema({
+  teamId: { type: Number, required: true },
+  lineup: {
+    goalkeepers: {
+      type: [Number],
+      required: [
+        true,
+        "Custom Error - Required Value *:Goalkeeper is required in lineup.",
+      ],
+    },
+    defenders: {
+      type: [Number],
+      required: [
+        true,
+        "Custom Error - Required Value *Defenders are required in lineup.",
+      ],
+    },
+    midfielders: {
+      type: [Number],
+      required: [
+        true,
+        "Custom Error - Required Value *:Midfielders are required in lineup.",
+      ],
+    },
+    strikers: {
+      type: [Number],
+      required: [
+        true,
+        "Custom Error - Required Value *:Strikers are required in lineup.",
+      ],
+    },
+    bench: {
+      type: [Number],
+      required: [
+        true,
+        "Custom Error - Required Value *:Bench Players are required in lineup.",
+      ],
+    },
+  },
+});
+
+const fixtureSchema = mongoose.Schema({
+  seasonId: {
+    type: String,
+    required: [true, "Custom Error - Required Value *:Season ID is required."],
+  },
+  gameweekId: {
+    type: Number,
+    required: [
+      true,
+      "Custom Error - Required Value *:Game week ID is required.",
+    ],
+  },
+  matchId: {
+    type: String,
+    required: [true, "Custom Error - Required Value *:Match ID is required."],
+  },
+  schedule: {
+    type: Date,
+    required: [
+      true,
+      "Custom Error - Required Value *:Fixture Schedule ID is required.",
+    ],
+  },
+  // Concern Here what are the possible values
+  status: { type: String, default: "scheduled" },
+  homeTeam: {
+    type: String,
+    required: [
+      true,
+      "Custom Error - Required Value *:Home Team is required for a fixture.",
+    ],
+  },
+  awayTeam: {
+    type: String,
+    required: [
+      true,
+      "Custom Error - Required Value *:Away Team is required for a fixture.",
+    ],
+  },
+  homeTeamLineUp: {
+    type: lineupSchema,
+  },
+  awayTeamLineUp: {
+    type: lineupSchema,
+  },
+  matchStat: matchStatSchema,
+});
+
+// Models
+const teamBkModel = bkConnection.model("teams", teamSchema);
+const playersBkModel = bkConnection.model("players", playerSchema);
+const fixtureBkModel = bkConnection.model("fixtures", fixtureSchema);
+
+module.exports = { teamBkModel, playersBkModel, fixtureBkModel };
