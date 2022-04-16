a0a854b5bb6b9cecdaf3dda58af2322183cbbcf2
nati-m-afw <it.natnael.mekonnen@gmail.com>
2022-04-16 14:10:22 +0300

    added touch support for drag n drop

--- vue-admin-client/src/main.js
+++ vue-admin-client/src/main.js
@@ -1,9 +1,15 @@
+import { polyfill } from "mobile-drag-drop";
+import { scrollBehaviourDragImageTranslateOverride } from "mobile-drag-drop/scroll-behaviour";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
 import errorHandler from "./error/error.middleware";
+
+polyfill({
+  dragImageTranslateOverride: scrollBehaviourDragImageTranslateOverride,
+});
 
 const app = createApp(App);
 app.config.errorHandler = errorHandler;
