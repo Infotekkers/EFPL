bf74f29054845637008f5926c1552c64b868d3e3
michael alemayehu <mikealexiv565@gmail.com>
2022-04-18 07:33:05 +0300

    styling and localisation for forgot and reset components

--- vue-admin-client/src/components/ForgotPasswordComponent.vue
+++ vue-admin-client/src/components/ForgotPasswordComponent.vue
@@ -1,13 +1,17 @@
 <template>
-  <div>
+  <div class="forgot-pass">
     <form @submit.prevent>
-      <h3>Forgot Password</h3>
+      <h3>{{ $t("forgot password") }}</h3>
       <div class="form-group">
-        <label> Reset link will be sent to the email you enter below </label>
-        <input type="email" required placeholder="Email" v-model="email" />
+        <label>
+          {{
+            $t("Reset link will be sent to the email you enter below")
+          }}</label
+        >
+        <input type="email" required v-model="email" />
       </div>
-      <button @click="handleSubmit">Submit</button>
     </form>
+    <button @click="handleSubmit">Submit</button>
     <p v-if="isLoading">Loading.....</p>
   </div>
 </template>
@@ -31,4 +35,56 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.forgot-pass {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 50%;
+}
+form {
+  text-align: left;
+  padding: var(--spacing-large);
+  border: 1px solid;
+  border-radius: 10px;
+}
+label {
+  color: var(--neutral-900);
+  display: inline-block;
+  margin: var(--spacing-regular) 0 var(--spacing-base);
+  font-size: var(--text-small);
+  font-weight: bold;
+  text-transform: uppercase;
+}
+input {
+  display: block;
+  padding: var(--spacing-small) var(--spacing-xsmall);
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid var(--neutral-600);
+  color: var(--neutral-600);
+}
+button {
+  background: var(--success-200);
+  color: var(--neutral-50);
+  border: 0;
+  padding: var(--spacing-small) var(--spacing-regular);
+  margin-top: var(--spacing-regular);
+  border-radius: 20px;
+  cursor: pointer;
+  width: 30%;
+}
+.submit {
+  text-align: center;
+}
+@media screen and (max-width: 768px) {
+  .forgot-pass {
+    width: 100%;
+  }
+  form {
+    border: none;
+  }
+}
+</style>
--- vue-admin-client/src/components/ResetPasswordComponent.vue
+++ vue-admin-client/src/components/ResetPasswordComponent.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
+  <div class="reset-pass">
     <form @submit.prevent="handleSubmit">
       <h3>Reset Your Password</h3>
       <div>
-        <label> Password </label>
+        <label> {{ $t("new") }} {{ $t("Password") }} </label>
         <input
           v-model="password"
           required
@@ -12,7 +12,7 @@
         />
       </div>
       <div>
-        <label>Confirm Your New Password </label>
+        <label> {{ $t("Password") }} {{ $t("Confirm") }}</label>
         <input
           v-model="password_confirm"
           required
@@ -23,7 +23,9 @@
 
       <button>Submit</button>
     </form>
-    <button @click="showPassword = !showPassword">show password</button>
+    <button @click="showPassword = !showPassword">
+      {{ $t("የይለፍ ቃል አሳይ") }}
+    </button>
     <div>{{ error }}</div>
   </div>
 </template>
@@ -51,3 +53,56 @@ export default {
   },
 };
 </script>
+<style scoped>
+.reset-pass {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 50%;
+}
+form {
+  text-align: left;
+  padding: var(--spacing-large);
+  border: 1px solid;
+  border-radius: 10px;
+}
+label {
+  color: var(--neutral-900);
+  display: inline-block;
+  margin: var(--spacing-regular) 0 var(--spacing-base);
+  font-size: var(--text-small);
+  font-weight: bold;
+  text-transform: uppercase;
+}
+input {
+  display: block;
+  padding: var(--spacing-small) var(--spacing-xsmall);
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid var(--neutral-600);
+  color: var(--neutral-600);
+}
+button {
+  background: var(--success-200);
+  color: var(--neutral-50);
+  border: 0;
+  padding: var(--spacing-small) var(--spacing-regular);
+  margin-top: var(--spacing-regular);
+  border-radius: 20px;
+  cursor: pointer;
+  width: 30%;
+}
+.submit {
+  text-align: center;
+}
+@media screen and (max-width: 768px) {
+  .reset-pass {
+    width: 100%;
+  }
+  form {
+    border: none;
+  }
+}
+</style>
