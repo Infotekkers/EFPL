72a841c1d07d3ea8295dd9d3da5936a2d6bdb56c
thomasalemayehu <61502533+thomasalemayehu@users.noreply.github.com>
2022-05-05 19:34:18 +0300

    Locale bug fix file removal

--- flutter-user-client/lib/application/fixture/fixture_event.dart
+++
@@ -1,6 +0,0 @@
-part of 'fixture_bloc.dart';
-
-@freezed
-class FixtureEvent with _$FixtureEvent {
-  const factory FixtureEvent.started() = _Started;
-}
--- flutter-user-client/lib/application/fixture/fixture_state.dart
+++
@@ -1,6 +0,0 @@
-part of 'fixture_bloc.dart';
-
-@freezed
-class FixtureState with _$FixtureState {
-  const factory FixtureState.initial() = _Initial;
-}
