b500faf767acf06913c1da608c3ee17d18cd1b60
michael alemayehu <mikealexiv565@gmail.com>
2022-05-15 20:15:24 +0300

    auth facade added

---
+++ flutter-user-client/lib/domain/auth/i_auth_facade.dart
@@ -0,0 +1,18 @@
+import 'package:dartz/dartz.dart';
+import 'package:efpl/domain/auth/auth_failure.dart';
+
+import 'auth_value_objects.dart';
+
+abstract class IAuthFacade {
+  // register
+  Future<Either<AuthFailure, Unit>> registerUser({
+    required EmailAddress emailAddress,
+    required Password password,
+  });
+  // login
+  Future<Either<AuthFailure, Unit>> signInUser({
+    required EmailAddress emailAddress,
+    required Password password,
+  });
+  // Future<void>signInWithGoogle();
+}
