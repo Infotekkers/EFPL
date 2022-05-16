62debef6509b15d1256b79c1063480ff60c04fad
michael alemayehu <mikealexiv565@gmail.com>
2022-05-16 15:05:37 +0300

    minor changes

--- flutter-user-client/lib/domain/auth/auth_value_objects.dart
+++ flutter-user-client/lib/domain/auth/auth_value_objects.dart
@@ -5,6 +5,7 @@ import 'package:efpl/domain/core/value_failures.dart';
 import '../core/value_validators.dart';
 
 class EmailAddress extends ValueObject<String> {
+  @override
   final Either<ValueFailure<String>, String> value;
 
   factory EmailAddress(String input) {
@@ -19,6 +20,7 @@ class EmailAddress extends ValueObject<String> {
 }
 
 class Password extends ValueObject<String> {
+  @override
   final Either<ValueFailure<String>, String> value;
 
   factory Password(String input) {
