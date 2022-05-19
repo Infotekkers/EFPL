8cfebc8d7cb04cd82f336affe44d6fae211096f4
jrermi <it.ermias.asmare@gmail.com>
2022-05-19 08:15:27 +0300

    create the league_table.dart

---
+++ flutter-user-client/lib/domain/league_table/league_table.dart
@@ -0,0 +1,22 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+@freezed
+abstract class LeagueTable with _$LeagueTable {
+  const factory LeagueTable({
+    required TeamName teamName,
+    required TeamLogo teamLogo,
+  }) = _LeagueTable;
+}
+
+@freezed
+abstract class TeamLeagueTable with _$TeamLeagueTable {
+  const factory TeamLeagueTable({
+    required TeamPoint teamPoint,
+    required TeamWon won,
+    required TeamLost lost,
+    required TeamDraw Draw,
+    required TeamGoalDifferntial goalDifferntial,
+    required TeamGoalAgainst goalAgainst,
+    required TeamGoalFor goalFor,
+  }) = _TeamLeagueTable;
+}
