9a61b112559247b99ef4f00afc587c8de5beccc7
thomasalemayehu <61502533+thomasalemayehu@users.noreply.github.com>
2022-05-31 21:02:21 +0300

    Added Token Injection

--- flutter-user-client/lib/services/socket.dart
+++ flutter-user-client/lib/services/socket.dart
@@ -1,5 +1,6 @@
 import 'package:efpl/application/fixture/fixture_bloc.dart';
 import 'package:efpl/injectable.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:socket_io_client/socket_io_client.dart' as io;
 
 class SocketApi {
@@ -11,7 +12,7 @@ class SocketApi {
 
   // Factory constructor to return same static instance every time you create any object.
   factory SocketApi() {
-    var socket = io.io("http://172.20.10.13:5000", <String, dynamic>{
+    var socket = io.io(dotenv.env["API"].toString(), <String, dynamic>{
       "transports": ["websocket"],
       "autoConnect": false,
     });
