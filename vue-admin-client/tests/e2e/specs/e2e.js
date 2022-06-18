56d6033ffadd4f91baa41470eb51413afca1620c
nati-m-afw <it.natnael.mekonnen@gmail.com>
2022-06-18 15:18:18 +0300

    e2e add player w fail scenarios

--- vue-admin-client/tests/e2e/specs/e2e.js
+++ vue-admin-client/tests/e2e/specs/e2e.js
@@ -57,168 +57,227 @@ describe("E2E Test", () => {
     cy.get('button[name="submit"]').click();
   });
 
-  it("Visits teams", () => {
-    cy.get('[href="/admin/teams"]').click();
-    cy.get('[class="teams-container"]')
-      .wait(3000)
+  // it("Visits teams", () => {
+  //   cy.get('[href="/admin/teams"]').click();
+  //   cy.get('[class="teams-container"]')
+  //     .wait(3000)
+  //     .children()
+  //     .should("have.length", 16);
+  // });
+
+  // it("Adds team", () => {
+  //   cy.get(".teams-add-new").click();
+
+  //   // Invalid team name
+  //   cy.get('input[name="teamName"]').clear().type("Sebeta City F.C");
+  //   cy.get('input[name="teamNameAmh"]').clear().type("ፈታኞቹ");
+  //   cy.get('input[name="teamCity"]').clear().type("Skypeia");
+  //   cy.get('input[name="teamStadium"]')
+  //     .clear({ force: true })
+  //     .type("Wellness Center", {
+  //       force: true,
+  //     });
+  //   cy.get('input[name="teamCoach"]').clear().type("Coach Margaret");
+  //   cy.get('input[name="stadiumCapacity"]').clear().type("200");
+  //   cy.get('input[name="foundedIn"]').clear().type("2022");
+  //   cy.get('input[name="teamLogo"]').attachFile("wellness_center_logo.png");
+  //   cy.get(".main-button-primary").click();
+  //   cy.get(".error-fill").should("exist");
+
+  //   // Empty logo
+  //   cy.get('input[name="teamName"]').clear().type("Testers");
+  //   cy.get('input[name="teamNameAmh"]').clear().type("ፈታኞቹ");
+  //   cy.get('input[name="teamCity"]').clear().type("Skypeia");
+  //   cy.get('input[name="teamStadium"]')
+  //     .clear({ force: true })
+  //     .type("Wellness Center", {
+  //       force: true,
+  //     });
+  //   cy.get('input[name="teamCoach"]').clear().type("Coach Margaret");
+  //   cy.get('input[name="stadiumCapacity"]').clear().type("200");
+  //   cy.get('input[name="foundedIn"]').clear().type("2022");
+  //   cy.get(".main-button-primary").click();
+  //   cy.get("input:invalid").should("have.length", 1);
+
+  //   // Valid data
+  //   cy.get('input[name="teamName"]').clear().type("Testers");
+  //   cy.get('input[name="teamNameAmh"]').clear().type("ፈታኞቹ");
+  //   cy.get('input[name="teamCity"]').clear().type("Skypeia");
+  //   cy.get('input[name="teamStadium"]')
+  //     .clear({ force: true })
+  //     .type("Wellness Center", {
+  //       force: true,
+  //     });
+  //   cy.get('input[name="teamCoach"]').clear().type("Coach Margaret");
+  //   cy.get('input[name="stadiumCapacity"]').clear().type("200");
+  //   cy.get('input[name="foundedIn"]').clear().type("2022");
+  //   cy.get('input[name="teamLogo"]').attachFile("wellness_center_logo.png");
+  //   cy.get(".main-button-primary").click();
+  //   cy.get(".main-modal-close").click();
+  // });
+
+  // it("Edits team", () => {
+  //   cy.get('[class="teams-container"]')
+  //     .contains("Testers")
+  //     .parent()
+  //     .find('[data-cp="edit-team"]')
+  //     .click();
+
+  //   // Invalid team name
+  //   cy.get('input[name="teamName"]').clear().type("Sebeta City F.C");
+  //   cy.get(".error-fill").should("exist");
+
+  //   // Empty logo
+  //   cy.get('input[name="teamName"]').clear().type("Testers");
+  //   cy.get(".team-modal-image-remove").click();
+  //   cy.get("input:invalid").should("have.length", 1);
+
+  //   // Valid data
+  //   cy.get('input[name="teamStadium"]')
+  //     .clear({ force: true })
+  //     .type("Legit Big Awesome Stadium");
+  //   cy.get('input[name="teamLogo"]').attachFile("wellness_center_logo.png");
+  //   cy.get(".main-button-primary").click();
+  //   cy.get(".main-modal-close").click();
+  // });
+
+  // it("Removes team", () => {
+  //   cy.get('[class="teams-container"]')
+  //     .contains("Testers")
+  //     .parent()
+  //     .find('[data-cp="delete-team"]')
+  //     .click();
+  // });
+
+  // it("Visits fixtures", () => {
+  //   cy.get('[href="/admin/fixtures"]').click();
+  //   cy.get('[class="fixture-main-container"]')
+  //     .wait(3000)
+  //     .should("have.length", 8);
+  // });
+
+  // it("Removes fixture", () => {
+  //   cy.get('[class="fixture-main-container"]')
+  //     .contains("Adama City S.C")
+  //     .parentsUntil('[class="fixture-main-container"]')
+  //     .find('[data-cp="delete-match"]')
+  //     .click();
+  // });
+
+  // it("Updates fixture", () => {
+  //   cy.get('[class="fixture-main-container"]')
+  //     .contains("Fasil Kenema S.C")
+  //     .parentsUntil('[class="fixture-main-container"]')
+  //     .find('[data-cp="edit-match"]')
+  //     .click();
+
+  //   // // Invalid away team
+  //   // cy.get(".fixture-away-team-next > .small-icon").click();
+  //   // cy.get('[class="fixture-modal-save-button"]').click();
+  //   // cy.get(".message-container").contains(
+  //   //   "Both home and away team are the same.(Adama City S.C)"
+  //   // );
+  //   // cy.get(".fixture-away-team-next > .small-icon").click();
+
+  //   // // Invalid home team
+  //   // cy.get(".fixture-home-team-next > .small-icon").click();
+  //   // cy.get('[class="fixture-modal-save-button"]').click();
+  //   // cy.get(".message-container").contains(
+  //   //   "Both home and away team are the same.(Saint George S.C)"
+  //   // );
+  //   // cy.get(".fixture-home-team-next > .small-icon").click();
+
+  //   // // Valid update
+  //   cy.get("#hour").select("16");
+  //   cy.get('[class="fixture-modal-save-button"]').click();
+  //   cy.get('[class="fixture-modal-close"]').click();
+  // });
+
+  // it("Adds new fixture", () => {
+  //   cy.get('[data-cp="add-gw"]').click();
+
+  //   // Invalid away team
+  //   cy.get(".fixture-away-team-next > .small-icon").click();
+  //   cy.get('[class="fixture-modal-save-button"]').click();
+  //   cy.get(".message-container").contains(
+  //     "Both home and away team are the same.(Adama City S.C)"
+  //   );
+  //   cy.get(".fixture-away-team-next > .small-icon").click();
+
+  //   // Invalid home team
+  //   cy.get(".fixture-home-team-next > .small-icon").click();
+  //   cy.get('[class="fixture-modal-save-button"]').click();
+  //   cy.get(".message-container").contains(
+  //     "Both home and away team are the same.(Saint George S.C)"
+  //   );
+  //   cy.get(".fixture-home-team-next > .small-icon").click();
+
+  //   // Valid home and away team
+  //   cy.get('[class="fixture-modal-save-button"]').click();
+  //   cy.get('[class="fixture-modal-close"]').click();
+  //   cy.get('[class="fixture-main-container"]')
+  //     // .wait(2000)
+  //     .should("have.length", 8);
+  // });
+
+  it("Visits players", () => {
+    cy.get('[href="/admin/players"]').click();
+    cy.get('[class="players-container"]')
+      // .wait(3000)
       .children()
-      .should("have.length", 16);
+      .should("have.length", 454);
   });
 
-  it("Adds team", () => {
-    cy.get(".teams-add-new").click();
+  it("Adds player", () => {
+    cy.get(".players-add-new").click();
 
-    // Invalid team name
-    cy.get('input[name="teamName"]').clear().type("Sebeta City F.C");
-    cy.get('input[name="teamNameAmh"]').clear().type("ፈታኞቹ");
-    cy.get('input[name="teamCity"]').clear().type("Skypeia");
-    cy.get('input[name="teamStadium"]')
-      .clear({ force: true })
-      .type("Wellness Center", {
-        force: true,
-      });
-    cy.get('input[name="teamCoach"]').clear().type("Coach Margaret");
-    cy.get('input[name="stadiumCapacity"]').clear().type("200");
-    cy.get('input[name="foundedIn"]').clear().type("2022");
-    cy.get('input[name="teamLogo"]').attachFile("wellness_center_logo.png");
+    // Invalid / Empty name
+    cy.get('input[name="playerName"]').clear();
+    cy.get('input[name="playerNameAmh"]').clear().type("ፈታኝ");
+    cy.get('select[name="PlayerTeam"]')
+      .last()
+      .select("Adama City S.C", { force: true });
+    cy.get('select[name="PlayerPosition"]').select("GoalKeepers", {
+      force: true,
+    });
+    cy.get('input[name="currentPrice"]').clear().type("5.5");
+    cy.get('select[name="injuryStatus"]').select("Available");
+    cy.get('input[name="injuryMessage"]').clear().type("Fit to play");
     cy.get(".main-button-primary").click();
     cy.get(".error-fill").should("exist");
 
-    // Empty logo
-    cy.get('input[name="teamName"]').clear().type("Testers");
-    cy.get('input[name="teamNameAmh"]').clear().type("ፈታኞቹ");
-    cy.get('input[name="teamCity"]').clear().type("Skypeia");
-    cy.get('input[name="teamStadium"]')
-      .clear({ force: true })
-      .type("Wellness Center", {
-        force: true,
-      });
-    cy.get('input[name="teamCoach"]').clear().type("Coach Margaret");
-    cy.get('input[name="stadiumCapacity"]').clear().type("200");
-    cy.get('input[name="foundedIn"]').clear().type("2022");
+    // Invalid price
+    cy.get('input[name="playerName"]').clear().type("Tester");
+    cy.get('input[name="playerNameAmh"]').clear().type("ፈታኝ");
+    cy.get('select[name="PlayerTeam"]')
+      .last()
+      .select("Adama City S.C", { force: true });
+    cy.get('select[name="PlayerPosition"]').select("GoalKeepers", {
+      force: true,
+    });
+    cy.get('input[name="currentPrice"]').clear().type("1");
+    cy.get('select[name="injuryStatus"]').select("Available");
+    cy.get('input[name="injuryMessage"]').clear().type("Fit to play");
     cy.get(".main-button-primary").click();
-    cy.get("input:invalid").should("have.length", 1);
+    cy.get(".error-fill").should("exist");
 
     // Valid data
-    cy.get('input[name="teamName"]').clear().type("Testers");
-    cy.get('input[name="teamNameAmh"]').clear().type("ፈታኞቹ");
-    cy.get('input[name="teamCity"]').clear().type("Skypeia");
-    cy.get('input[name="teamStadium"]')
-      .clear({ force: true })
-      .type("Wellness Center", {
-        force: true,
-      });
-    cy.get('input[name="teamCoach"]').clear().type("Coach Margaret");
-    cy.get('input[name="stadiumCapacity"]').clear().type("200");
-    cy.get('input[name="foundedIn"]').clear().type("2022");
-    cy.get('input[name="teamLogo"]').attachFile("wellness_center_logo.png");
+    cy.get('input[name="playerName"]').clear().type("Tester");
+    cy.get('input[name="playerNameAmh"]').clear().type("ፈታኝ");
+    cy.get('select[name="PlayerTeam"]')
+      .last()
+      .select("Adama City S.C", { force: true });
+    cy.get('select[name="PlayerPosition"]').select("GoalKeepers", {
+      force: true,
+    });
+    cy.get('input[name="currentPrice"]').clear().type("5.5");
+    cy.get('select[name="injuryStatus"]').select("Available");
+    cy.get('input[name="injuryMessage"]').clear().type("Fit to play");
     cy.get(".main-button-primary").click();
     cy.get(".main-modal-close").click();
   });
 
-  it("Edits team", () => {
-    cy.get('[class="teams-container"]')
-      .contains("Testers")
-      .parent()
-      .find('[data-cp="edit-team"]')
-      .click();
-
-    // Invalid team name
-    cy.get('input[name="teamName"]').clear().type("Sebeta City F.C");
-    cy.get(".error-fill").should("exist");
-
-    // Empty logo
-    cy.get('input[name="teamName"]').clear().type("Testers");
-    cy.get(".team-modal-image-remove").click();
-    cy.get("input:invalid").should("have.length", 1);
-
-    // Valid data
-    cy.get('input[name="teamStadium"]')
-      .clear({ force: true })
-      .type("Legit Big Awesome Stadium");
-    cy.get('input[name="teamLogo"]').attachFile("wellness_center_logo.png");
-    cy.get(".main-button-primary").click();
-    cy.get(".main-modal-close").click();
-  });
-
-  it("Removes team", () => {
-    cy.get('[class="teams-container"]')
-      .contains("Testers")
-      .parent()
-      .find('[data-cp="delete-team"]')
-      .click();
-  });
-
-  it("Visits fixtures", () => {
-    cy.get('[href="/admin/fixtures"]').click();
-    cy.get('[class="fixture-main-container"]')
-      .wait(3000)
-      .should("have.length", 8);
-  });
-
-  it("Removes fixture", () => {
-    cy.get('[class="fixture-main-container"]')
-      .contains("Adama City S.C")
-      .parentsUntil('[class="fixture-main-container"]')
-      .find('[data-cp="delete-match"]')
-      .click();
-  });
-
-  it("Updates fixture", () => {
-    cy.get('[class="fixture-main-container"]')
-      .contains("Fasil Kenema S.C")
-      .parentsUntil('[class="fixture-main-container"]')
-      .find('[data-cp="edit-match"]')
-      .click();
-
-    // // Invalid away team
-    // cy.get(".fixture-away-team-next > .small-icon").click();
-    // cy.get('[class="fixture-modal-save-button"]').click();
-    // cy.get(".message-container").contains(
-    //   "Both home and away team are the same.(Adama City S.C)"
-    // );
-    // cy.get(".fixture-away-team-next > .small-icon").click();
-
-    // // Invalid home team
-    // cy.get(".fixture-home-team-next > .small-icon").click();
-    // cy.get('[class="fixture-modal-save-button"]').click();
-    // cy.get(".message-container").contains(
-    //   "Both home and away team are the same.(Saint George S.C)"
-    // );
-    // cy.get(".fixture-home-team-next > .small-icon").click();
-
-    // // Valid update
-    cy.get("#hour").select("16");
-    cy.get('[class="fixture-modal-save-button"]').click();
-    cy.get('[class="fixture-modal-close"]').click();
-  });
-
-  it("Adds new fixture", () => {
-    cy.get('[data-cp="add-gw"]').click();
-
-    // Invalid away team
-    cy.get(".fixture-away-team-next > .small-icon").click();
-    cy.get('[class="fixture-modal-save-button"]').click();
-    cy.get(".message-container").contains(
-      "Both home and away team are the same.(Adama City S.C)"
-    );
-    cy.get(".fixture-away-team-next > .small-icon").click();
-
-    // Invalid home team
-    cy.get(".fixture-home-team-next > .small-icon").click();
-    cy.get('[class="fixture-modal-save-button"]').click();
-    cy.get(".message-container").contains(
-      "Both home and away team are the same.(Saint George S.C)"
-    );
-    cy.get(".fixture-home-team-next > .small-icon").click();
-
-    // Valid home and away team
-    cy.get('[class="fixture-modal-save-button"]').click();
-    cy.get('[class="fixture-modal-close"]').click();
-    cy.get('[class="fixture-main-container"]')
-      // .wait(2000)
-      .should("have.length", 8);
-  });
+  it("Removes Player", () => {});
 
   // it("Visits fixture details", () => {
   //   cy.get('[class="fixture-main-container"]')
@@ -303,14 +362,6 @@ describe("E2E Test", () => {
   //   cy.get("#goals > :nth-child(4)").click();
   //   cy.get(".action-primary").click();
   //   cy.get("#close > button").wait(1000).click();
-  // });
-
-  // it("Visits players", () => {
-  //   cy.get('[href="/admin/players"]').click();
-  //   cy.get('[class="players-container"]')
-  //     .wait(3000)
-  //     .children()
-  //     .should("have.length", 454);
   // });
 
   it("Logs out", () => {
