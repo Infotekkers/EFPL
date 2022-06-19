9c7ba780a86e2d54d9e0314c5e43c50bdfc7319d
nati-m-afw <it.natnael.mekonnen@gmail.com>
2022-06-19 11:52:05 +0300

    positional container gk separation

--- flutter-user-client/replace.dart
+++ flutter-user-client/replace.dart
@@ -1,10 +1,10 @@
-  // final initialTransferMIDKey1 =
-  //       find.byValueKey("initialTransferViewMIDKey1");
-  //   final initialTransferMIDKey2 =
-  //       find.byValueKey("initialTransferViewMIDKey2");
-  //   final initialTransferMIDKey3 =
-  //       find.byValueKey("initialTransferViewMIDKey1");
-  //   final initialTransferMIDKey4 =
-  //       find.byValueKey("initialTransferViewMIDKey2");
-  //   final initialTransferMIDKey5 =
-  //       find.byValueKey("initialTransferViewMIDKey2");
+// final initialTransferMIDKey1 =
+//       find.byValueKey("initialTransferViewMIDKey1");
+//   final initialTransferMIDKey2 =
+//       find.byValueKey("initialTransferViewMIDKey2");
+//   final initialTransferMIDKey3 =
+//       find.byValueKey("initialTransferViewMIDKey1");
+//   final initialTransferMIDKey4 =
+//       find.byValueKey("initialTransferViewMIDKey2");
+//   final initialTransferMIDKey5 =
+//       find.byValueKey("initialTransferViewMIDKey2");
